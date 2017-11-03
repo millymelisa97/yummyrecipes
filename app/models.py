f61742a8e0fd7996adfb27403ac8f317f699bb4f
millymelisa97 <millymelisa97@gmail.com>
2017-11-03 16:31:43 +0300

    Implemeted user registration

--- app/models.py
+++ app/models.py
@@ -1,8 +1,19 @@
 class User:
-    # variable to store all users registered
     all_users = dict()
 
     def __init__(self, username, password):
-        """User initializer"""
         self.username = username
         self.password = password
+
+    def register(self):
+        """ Registers a user """
+        if str(self.username) not in User.all_users:
+            User.all_users[str(self.username)] = self.get_details()
+            return self.get_details()
+        return False
+
+    def get_details(self):
+        return {
+            "username": self.username,
+            "password": self.password
+        }
